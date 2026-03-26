7dce08cd7c89c29e66f9f11655bded87fa5de30e
KrimsN <deni.tim99@yandex.ru>
2026-03-26 22:39:56 +0700

    init hello world

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+
+plugindir = Path.absolute(Path(__file__).parent)
+paths = (".", "lib", "plugin")
+sys.path = [str(plugindir / p) for p in paths] + sys.path
+
+from flowlauncher import FlowLauncher
+import webbrowser
+
+class Test(FlowLauncher):
+    def query(self, query):
+        return [
+            {
+                "Title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
+                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
+                "IcoPath": "Images/app.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
+                }
+            }
+        ]
+
+    def context_menu(self, data):
+        return [
+            {
+                "Title": "Hello World Python's Context menu",
+                "SubTitle": "Press enter to open Flow the plugin's repo in GitHub",
+                "IcoPath": "Images/app.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher.Plugin.HelloWorldPython"]
+                }
+            }
+        ]
+
+    def open_url(self, url):
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    Test()
